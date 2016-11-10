fb3c4c05da3f233ee408d376523492e6a742bfa0
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-11-09 23:38:48 -0600

    Scripts reciclados de usuario vs boss

---
+++ LostWorld/BossTemplate/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public DiverLifeController diverLife;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (diverLife.life <= 0) {
+			Debug.Log("Die Message");
+		}
+	}
+}
---
+++ LostWorld/BossTemplate/HarpoonMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarpoonMovement : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	private Rigidbody2D rigidBody2D;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		rigidBody2D = GetComponent<Rigidbody2D>();
+		if (spriteRenderer.flipX)
+			rigidBody2D.velocity = new Vector2(1.0f * speed, 0.0f);
+		else 
+			rigidBody2D.velocity = new Vector2(-1.0f * speed, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigidBody2D.position.x < -15 || rigidBody2D.position.x > 15)
+			Destroy(this.gameObject);
+	}
+}
