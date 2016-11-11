21da416653030ba70af36f5022f70710cbc1d516
marcobravociencias <marcobravo@ciencias.unam.mx>
2016-11-10 20:44:04 -0600

    Fondo y Mov Camara

---
+++ LostWorld/Assets/script/MovMapa.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovMapa : MonoBehaviour {
+	public Vector2 speed = new Vector2(1, 1);
+ 	public Vector2 direction = new Vector2(0,0);
+  	public bool isLinkedToCamera = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+  {
+    // Movement
+    //Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+
+  	float movX = Input.GetAxis ("Horizontal");
+	float movY = Input.GetAxis ("Vertical");
+
+	Vector3 movement = new Vector3(speed.x * movX, speed.y * movY, 0);
+
+    movement *= Time.deltaTime;
+    transform.Translate(movement);
+
+    // Move the camera
+    if (isLinkedToCamera)
+    {
+      Camera.main.transform.Translate(movement);
+    }
+  }
+}
