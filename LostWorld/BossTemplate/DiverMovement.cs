dbf0e2ee0b6850ab309b0a7c0100125cbd666a07
marcobravociencias <marcobravo@ciencias.unam.mx>
2016-11-14 21:15:03 -0600

    Codigo para que dios Zeus me perdone

---
+++ LostWorld/BossTemplate/DiverMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiverMovement : MonoBehaviour {
+
+	public float speed;
+	public float xMin;
+	public float xMax;
+	public float yMin;
+	public float yMax;
+	public bool isFacingLeft;
+	public bool isFacingRight;
+
+	//private Animator animator;
+	private Rigidbody2D rigidBody2D;
+	private SpriteRenderer spriteRenderer;
+	private Component[] weapon;
+	private DiverLifeController life;
+
+	// Use this for initialization
+	void Start () {
+		speed = 8.0f;
+		xMin = -15.0f;
+		xMax = 15.0f;
+		yMin = -5.5f;
+		yMax = 5.5f;
+		isFacingLeft = false;
+		isFacingRight = true;
+		
+		rigidBody2D = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		weapon = GetComponentsInChildren<WeaponController>();
+		life = GetComponent<DiverLifeController>();
+	}
+
+	void FixedUpdate () {
+		if (life.life <= 0) {
+			rigidBody2D.velocity = Vector2.zero;
+			return;
+		}
+		
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveX, moveY);
+		rigidBody2D.velocity = movement * speed;
+		rigidBody2D.position = new Vector2 (
+			Mathf.Clamp(rigidBody2D.position.x, xMin, xMax), 
+            Mathf.Clamp(rigidBody2D.position.y, yMin, yMax)
+		);
+
+		if (moveX < 0 && isFacingLeft)
+			FlipRight();
+		else if (moveX > 0 && isFacingRight)
+			FlipLeft();
+	}
+
+	private void FlipLeft () {
+		isFacingLeft = true;
+		isFacingRight = false;
+		spriteRenderer.flipX = true;
+		
+		foreach (WeaponController w in weapon) {
+			w.isFacingLeft = true;
+			w.isFacingRight = false;
+		}
+	}
+
+	private void FlipRight () {
+		isFacingLeft = false;
+		isFacingRight = true;
+		spriteRenderer.flipX = false;
+
+		foreach (WeaponController w in weapon) {
+			w.isFacingLeft = false;
+			w.isFacingRight = true;
+		}
+	}
+}
---
+++ LostWorld/BossTemplate/WeaponController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+	public Transform harpoon;
+	public float shootRate;
+	public bool isFacingLeft;
+	public bool isFacingRight;
+	
+	private float nextShoot;
+	private Animator animator;
+	private DiverMovement diver;
+	private SpriteRenderer harpoonSR;
+
+	// Use this for initialization
+	void Start () {
+		shootRate = 2.5f;
+		isFacingLeft = true;
+		isFacingRight = false;
+
+		animator = transform.parent.GetComponent<Animator>();
+		harpoonSR = harpoon.GetComponent<SpriteRenderer>();
+		diver = transform.parent.GetComponent<DiverMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(InTimeAnimations() );
+	}
+
+	private IEnumerator InTimeAnimations () {
+		 if (Input.GetButtonDown("Fire1") && Time.time > nextShoot) {
+			nextShoot = Time.time + shootRate;
+			animator.SetTrigger("useHarpoon");
+			yield return new WaitForSeconds (0.5f);
+			if (isFacingLeft)
+				harpoonSR.flipX = true;
+			else
+				harpoonSR.flipX = false;
+			Instantiate(harpoon, transform.position, transform.rotation);
+		}
+ 	}
+}
