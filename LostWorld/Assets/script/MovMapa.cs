1661bb9f01fcac751329c4c4e356600741bd6acc
marcobravociencias <marcobravo@ciencias.unam.mx>
2016-11-14 20:49:08 -0600

    Colliders Casa y bosque, cambio sprite al caminar.

--- LostWorld/Assets/script/MovMapa.cs
+++ LostWorld/Assets/script/MovMapa.cs
@@ -3,32 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovMapa : MonoBehaviour {
-	public Vector2 speed = new Vector2(1, 1);
- 	public Vector2 direction = new Vector2(0,0);
-  	public bool isLinkedToCamera = false;
+	public float velocidad = 0.3f;
+  	public bool seMueveCamara = false;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update()
-  {
-    // Movement
-    //Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+	void Update() {
+	    // Movement
+	    //Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
 
-  	float movX = Input.GetAxis ("Horizontal");
-	float movY = Input.GetAxis ("Vertical");
+	  	float movX = Input.GetAxis ("Horizontal");
+		float movY = Input.GetAxis ("Vertical");
 
-	Vector3 movement = new Vector3(speed.x * movX, speed.y * movY, 0);
+			Vector3 movement = new Vector3(velocidad* movX, velocidad * movY, 0);
 
-    movement *= Time.deltaTime;
-    transform.Translate(movement);
+	    movement *= Time.deltaTime;
+	    transform.Translate(movement);
 
-    // Move the camera
-    if (isLinkedToCamera)
-    {
-      Camera.main.transform.Translate(movement);
-    }
-  }
+	    // Move the camera
+	    if (seMueveCamara)
+	    {
+	      Camera.main.transform.Translate(movement);
+	    }
+  	}
 }
---
+++ LostWorld/Assets/script/SpriteJugador.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteJugador : MonoBehaviour {
+	public Sprite sprite0; // Drag your first sprite here
+	public Sprite sprite1; // Drag your second sprite here
+	public Sprite sprite2; // Drag your first sprite here
+	public Sprite sprite3; // Drag your second sprite here
+	public Sprite sprite4; // Drag your first sprite here
+	public Sprite sprite5; // Drag your second sprite here
+	public Sprite sprite6; // Drag your first sprite here
+	public Sprite sprite7; // Drag your second sprite here
+	public Sprite sprite8; // Drag your first sprite here
+	public float velocidadCambio = 0.2f;
+	private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+    	if (spriteRenderer.sprite == null){
+    		spriteRenderer.sprite = sprite0;
+    	}
+    	StartCoroutine (Movimiento ());		
+	}
+     
+    IEnumerator Movimiento () {
+         while (true) {
+			if (Input.GetAxis ("Vertical") != 0){
+				if (Input.GetAxis ("Vertical") < 0) {
+					CambiaAbajo();	
+					yield return new WaitForSeconds(velocidadCambio);
+				}else if (Input.GetAxis ("Vertical") > 0) {
+					CambiaArriba();	
+					yield return new WaitForSeconds(velocidadCambio);
+				}
+			}else{
+				if (Input.GetAxis ("Horizontal") < 0) {
+					CambiaIzquierda();	
+					yield return new WaitForSeconds(velocidadCambio);
+				}else if (Input.GetAxis ("Horizontal") > 0) {
+					CambiaDerecha();	
+					yield return new WaitForSeconds(velocidadCambio);
+				}	
+			}
+			yield return new WaitForSeconds(velocidadCambio*0);
+        }
+        //yield return new WaitForSeconds(velocidadCambio);
+    }
+
+	void CambiaAbajo (){
+		spriteRenderer.flipX = false;
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			spriteRenderer.sprite = sprite0;	
+		}
+
+		if (Input.GetAxis ("Vertical") < 0) {
+			//spriteRenderer.sprite = sprite0;
+			if (spriteRenderer.sprite == sprite0) {
+				spriteRenderer.sprite = sprite1;
+			} else if (spriteRenderer.sprite == sprite1) {
+				spriteRenderer.sprite = sprite2;
+			} else if (spriteRenderer.sprite == sprite2) {
+				spriteRenderer.sprite = sprite1;
+			}	
+		}
+		if (Input.GetAxis ("Vertical") == 0) {
+			spriteRenderer.sprite = sprite0;
+		}
+	}
+
+	void CambiaArriba (){
+		spriteRenderer.flipX = false;
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			spriteRenderer.sprite = sprite3;	
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			//spriteRenderer.sprite = sprite0;
+			if (spriteRenderer.sprite == sprite3) {
+				spriteRenderer.sprite = sprite4;
+			} else if (spriteRenderer.sprite == sprite4) {
+				spriteRenderer.sprite = sprite5;
+			} else if (spriteRenderer.sprite == sprite5) {
+				spriteRenderer.sprite = sprite4;
+			}	
+		}
+		if (Input.GetAxis ("Vertical") == 0 ) {
+			spriteRenderer.sprite = sprite3;
+		}
+	}
+
+	void CambiaDerecha (){
+		spriteRenderer.flipX = true;
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			spriteRenderer.sprite = sprite6;	
+		}
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			//spriteRenderer.sprite = sprite0;
+			if (spriteRenderer.sprite == sprite6) {
+				spriteRenderer.sprite = sprite7;
+			} else if (spriteRenderer.sprite == sprite7) {
+				spriteRenderer.sprite = sprite8;
+			} else if (spriteRenderer.sprite == sprite8) {
+				spriteRenderer.sprite = sprite7;
+			}	
+		}
+		if (Input.GetAxis ("Horizontal") == 0 ) {
+			spriteRenderer.sprite = sprite6;
+		}
+		//spriteRenderer.flipX = false;
+	}
+
+	void CambiaIzquierda (){
+		spriteRenderer.flipX = false;
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			spriteRenderer.sprite = sprite6;	
+		}
+
+		if (Input.GetAxis ("Horizontal") < 0) {
+			//spriteRenderer.sprite = sprite0;
+			if (spriteRenderer.sprite == sprite6) {
+				spriteRenderer.sprite = sprite7;
+			} else if (spriteRenderer.sprite == sprite7) {
+				spriteRenderer.sprite = sprite8;
+			} else if (spriteRenderer.sprite == sprite8) {
+				spriteRenderer.sprite = sprite7;
+			}	
+		}
+		if (Input.GetAxis ("Horizontal") == 0 ) {
+			spriteRenderer.sprite = sprite6;
+		}
+	}
+}
